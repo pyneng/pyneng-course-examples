941b5e7d8ae778427e15070c00561772024cf9ea
pyneng <pyneng.course@gmail.com>
2022-09-21 09:01:00 +0000

    UPD 06 new examples

---
+++ examples/06_control_structures/basics_if_01.py
@@ -0,0 +1,16 @@
+
+# value > 8
+# value >= 4
+# value < 100
+# value <= 100
+# value == 10
+# value != 10
+
+value = 100
+
+if value == 10:
+    print("value равно 10")
+elif value < 10:
+    print("value < 10")
+else:
+    print("value > 10")
