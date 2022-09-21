3f430e8b9fb9c5ef90d8bdb50dc15831e5f56459
pyneng <pyneng.course@gmail.com>
2022-09-21 12:11:17 +0000

    UPD 06 new examples

--- examples/06_control_structures/examples_for_01_str_to_int.py
+++ examples/06_control_structures/examples_for_01_str_to_int.py
@@ -1,2 +1,9 @@
+from pprint import pprint
 vlans = ["1", "2", "3", "4", "5"]
 
+vlans_list = []
+for vl in vlans:
+    new_vl = int(vl)
+    vlans_list.append(new_vl)
+
+print(vlans_list)
--- examples/06_control_structures/examples_for_02_str_to_int_change_list.py
+++ examples/06_control_structures/examples_for_02_str_to_int_change_list.py
@@ -1,2 +1,11 @@
-vlans = ["1", "2", "3", "4", "5"]
+from pprint import pprint
+vlans = ["10", "20", "30", "40", "50"]
 
+index_range = range(len(vlans))
+
+print(vlans)
+
+for index in index_range:
+    vlans[index] = int(vlans[index])
+
+print(vlans)
