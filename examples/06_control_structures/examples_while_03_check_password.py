d53e6528af91031d79402a91289152691fc6b1e7
pyneng <pyneng.course@gmail.com>
2022-09-21 13:35:09 +0000

    UPD 06 new examples

--- examples/06_control_structures/examples_while_03_check_password.py
+++ examples/06_control_structures/examples_while_03_check_password.py
@@ -2,16 +2,16 @@ from pprint import pprint
 
 username = input("Введите имя пользователя: ")
 password = input("Введите пароль: ")
-pprint(username)
-pprint(password)
 
 numbers = set("0123456789")
 
-if len(password) < 8:
-    print("Пароль слишком короткий")
-elif username.lower() in password.lower():
-    print("В пароле содержится имя пользователя")
-elif len(numbers.intersection(set(password))) < 3:
-    print("В пароле должны быть хотя 3 цифры")
-else:
-    print(f"Пароль для пользователя {username} установлен")
+while (
+    len(password) < 8
+    or username.lower() in password.lower()
+    or len(numbers.intersection(set(password))) < 3
+):
+    print("Пароль не прошел проверки")
+    password = input("Введите пароль еще раз: ")
+
+
+print(f"Пароль для пользователя {username} установлен")
