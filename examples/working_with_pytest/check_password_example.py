6c5278115fb343714cd7ceb7c65ea841632871ec
pyneng <pyneng.course@gmail.com>
2022-10-11 14:51:19 +0000

    DEL old examples

--- examples/working_with_pytest/check_password_example.py
+++
@@ -1,9 +0,0 @@
-def check_password(username, password, password_length=8,
-                   check_username=True, spec_sym=True):
-    if len(password) < password_length:
-        return False
-    elif check_username and username in password:
-        return False
-    else:
-        return True
-
