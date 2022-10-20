66eecc7987bf7ebd783d852f956bb27344519557
pyneng <pyneng.course@gmail.com>
2022-10-20 16:14:47 +0000

    14, 15

--- examples/13_iterator_generator/enumerate_eem.py
+++
@@ -1,7 +0,0 @@
-import sys
-
-config = sys.argv[1]
-
-with open(config, "r") as file:
-    for (i, command) in enumerate(file, 1):
-        print('action {:04} cli command "{}"'.format(i, command.rstrip()))
