c69e244ce49c0daac6fe66b581f0130823e18733
pyneng <pyneng.course@gmail.com>
2022-11-07 19:20:33 +0000

    update 18 examples

---
+++ examples/18_ssh_telnet/example_03_netmiko_cfg_errors.py
@@ -0,0 +1,24 @@
+from pprint import pprint
+from netmiko import Netmiko, NetmikoBaseException, ConfigInvalidException
+from paramiko.ssh_exception import SSHException
+import yaml
+
+
+def configure_net_devices(device_params, commands, error_pattern="%"):
+    try:
+        with Netmiko(**device_params) as ssh:
+            ssh.enable()
+            cmd_output = ssh.send_config_set(commands, error_pattern=error_pattern)
+        return cmd_output
+    except ConfigInvalidException:
+        raise
+    except (NetmikoBaseException, SSHException) as error:
+        print(error)
+
+
+if __name__ == "__main__":
+    with open("devices.yaml") as f:
+        device_list = yaml.safe_load(f)
+    r1 = device_list[0]
+    result = configure_net_devices(r1, "loging 10.1.1.1")
+    pprint(result)
