f078351e9d4de8440bfd3c609614437c33235adc
pyneng <pyneng.course@gmail.com>
2022-11-09 18:32:49 +0000

    update 18 examples

---
+++ examples/18_ssh_telnet/example_12_telnetlib_read_very_eager.py
@@ -0,0 +1,30 @@
+from pprint import pprint
+import telnetlib
+import time
+
+
+def get_cisco_show_output(host, username, password, enable_pass, command):
+    with telnetlib.Telnet(host) as t:
+        t.read_until(b'Username:')
+        t.write(f"{username}\n".encode("utf-8"))
+        t.read_until(b'Password')
+        t.write(f"{password}\n".encode("utf-8"))
+        t.read_until(b'>')
+
+        t.write(b"enable\n")
+        t.read_until(b'Password')
+        t.write(f"{enable_pass}\n".encode("utf-8"))
+        t.read_until(b'#')
+        t.write(b"terminal length 0\n")
+        t.read_until(b'#', timeout=5)
+
+        t.write(f"{command}\n".encode("utf-8"))
+        time.sleep(1)
+        boutput = t.read_very_eager()
+        output = boutput.decode("utf-8").replace("\r\n", "\n")
+        return output
+
+
+if __name__ == "__main__":
+    out = get_cisco_show_output("192.168.100.1", "cisco", "cisco", "cisco", "sh ip int br")
+    pprint(out, width=120)
