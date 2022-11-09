caa7c3e65573077acd9277ed5ac4ec1bd0e79d9e
pyneng <pyneng.course@gmail.com>
2022-11-09 12:30:29 +0000

    update 18 examples

--- examples/18_ssh_telnet/example_11_telnetlib_connection.py
+++ examples/18_ssh_telnet/example_11_telnetlib_connection.py
@@ -1,8 +1,7 @@
 from pprint import pprint
 import telnetlib
 
-
-def cisco_connection(host, username, password, enable_pass):
+def cisco_telnet_connection(host, username, password, enable_pass):
     try:
         t = telnetlib.Telnet(host, timeout=5)
         t.read_until(b'Username:')
@@ -18,22 +17,20 @@ def cisco_connection(host, username, password, enable_pass):
             if index in (1, -1):
                 raise ConnectionError(f"Enable authentication failed on device {host}")
         elif index == 1:
+            # print(">>> enable")
             pass
         else:
             raise ConnectionError(f"Authentication failed on device {host}")
-
-        t.write(b"terminal length 0\n")
-        t.read_until(b'#', timeout=5)
         return t
-
     except OSError as error:
         print(error)
 
 
 def configure_cisco(host, username, password, enable_pass, commands):
-    conn = cisco_connection(host, username, password, enable_pass)
-    if not conn:
+    conn = cisco_telnet_connection(host, username, password, enable_pass)
+    if conn is None:
         return
+
     with conn as t:
         if type(commands) == str:
             commands = ["conf t", commands, "end"]
@@ -47,6 +44,24 @@ def configure_cisco(host, username, password, enable_pass, commands):
         return output
 
 
+def get_cisco_show_output(host, username, password, enable_pass, command):
+    conn = cisco_telnet_connection(host, username, password, enable_pass)
+    if conn is None:
+        return
+
+    with conn as t:
+        t.write(b"terminal length 0\n")
+        boutput = t.read_until(b'#', timeout=5)
+
+        t.write(f"{command}\n".encode("utf-8"))
+        boutput = t.read_until(b'#', timeout=5)
+        output = boutput.decode("utf-8").replace("\r\n", "\n")
+        return output
+
+
 if __name__ == "__main__":
+    out = get_cisco_show_output("192.168.100.1", "cisco", "cisco", "cisco", "sh ip int br")
+    pprint(out, width=120)
+
     out = configure_cisco("192.168.100.1", "cisco", "cisco", "cisco", ["int lo42", "desc TEST"])
     pprint(out, width=120)
