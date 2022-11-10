812094a69f63811a54c2fffa26ec5f04d3414f6b
pyneng <pyneng.course@gmail.com>
2022-11-10 12:47:45 +0000

    update 18 examples

--- examples/18_ssh_telnet/example_15_paramiko_read_until.py
+++ examples/18_ssh_telnet/example_15_paramiko_read_until.py
@@ -6,51 +6,48 @@ import re
 
 def cisco_send_show_command(
     host, username, password, enable_pass, command, max_read=60000,
-    pause=0.5,
+    pause=0.5, prompt="#", read_timeout=5
 ):
-    client = paramiko.SSHClient()
-    client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-    client.connect(
-        hostname=host,
-        username=username,
-        password=password,
-        look_for_keys=False,
-        allow_agent=False,
-    )
-    with client.invoke_shell() as ssh:
+    with paramiko.SSHClient() as client:
+        client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        client.connect(
+            hostname=host,
+            username=username,
+            password=password,
+            look_for_keys=False,
+            allow_agent=False,
+        )
+        ssh = client.invoke_shell()
         ssh.settimeout(2)
-
         ssh.send("enable\n")
         ssh.send(f"{enable_pass}\n")
         time.sleep(pause)
-        ssh.recv(max_read)
         ssh.send(f"terminal length 0\n")
         time.sleep(pause)
-        read_output = ssh.recv(max_read).decode("utf-8")
-        prompt = re.search(r"\S+#", read_output).group()
+        ssh.recv(max_read)
 
         ssh.send(f"{command}\n")
+        ssh.settimeout(read_timeout)
         output = ""
-        ssh.settimeout(5)
         while True:
-            time.sleep(pause)
+            time.sleep(0.2)
             try:
-                part = ssh.recv(max_read).decode("utf-8")
+                part = ssh.recv(100).decode("utf-8")
+                pprint(part)
             except OSError:
                 break
+            print("=" * 40)
             output += part
-            if prompt in part:
+            if prompt in output:
                 break
-        return output
+
+        return output.replace("\r\n", "\n")
 
 
 if __name__ == "__main__":
-    result = {}
-    ip_list = ["192.168.100.1", "192.168.100.2", "192.168.100.3"]
-    for ip in ip_list:
-        out = cisco_send_show_command(
-            ip, "cisco", "cisco", "cisco", "sh ip int br"
-        )
-        result[ip] = out
-        break
-    pprint(result)
+    out = cisco_send_show_command(
+        "192.168.100.1", "cisco", "cisco", "cisco", "ping 8.8.8.8 repeat 5",
+        prompt="#", read_timeout=10
+    )
+    pprint(out, width=120)
+
--- examples/18_ssh_telnet/example_16_paramiko_read_until_function.py
+++ examples/18_ssh_telnet/example_16_paramiko_read_until_function.py
@@ -1,65 +1,59 @@
 import paramiko
 import time
 from pprint import pprint
-import socket
 import re
 
 
-def cisco_send_show_commands(
-    host, username, password, enable_pass, commands, max_read=60000,
-    pause=0.5,
+def cisco_send_show_command(
+    host, username, password, enable_pass, command, max_read=60000,
+    pause=0.5, prompt="#", read_timeout=5
 ):
-    client = paramiko.SSHClient()
-    client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-    client.connect(
-        hostname=host,
-        username=username,
-        password=password,
-        look_for_keys=False,
-        allow_agent=False,
-    )
-    with client.invoke_shell() as ssh:
+    with paramiko.SSHClient() as client:
+        client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        client.connect(
+            hostname=host,
+            username=username,
+            password=password,
+            look_for_keys=False,
+            allow_agent=False,
+        )
+        ssh = client.invoke_shell()
         ssh.settimeout(2)
-
         ssh.send("enable\n")
         ssh.send(f"{enable_pass}\n")
         time.sleep(pause)
-        ssh.recv(max_read)
         ssh.send(f"terminal length 0\n")
         time.sleep(pause)
-        read_output = ssh.recv(max_read).decode("utf-8")
-        prompt = re.search(r"\S+#", read_output).group()
+        ssh.recv(max_read)
 
-        if type(commands) == str:
-            commands = [commands]
-        result = {}
-        for cmd in commands:
-            ssh.send(f"{cmd}\n")
-            output = read_until_prompt(ssh, prompt)
-            result[cmd] = output
-        return result
+        ssh.send(f"{command}\n")
+        output = _read_until_prompt(ssh, prompt, read_timeout, max_read)
+
+        return output.replace("\r\n", "\n")
 
 
-def read_until_prompt(ssh, prompt, pause=0.2, max_read=60000):
-    command_output = ""
-    ssh.settimeout(5)
+def _read_until_prompt(ssh, prompt, read_timeout, max_read):
+    ssh.settimeout(read_timeout)
+    output = ""
     while True:
-        time.sleep(pause)
+        time.sleep(0.2)
         try:
-            part = ssh.recv(max_read).decode("utf-8")
-        except socket.timeout:
+            part = ssh.recv(100).decode("utf-8")
+            # part = ssh.recv(max_read).decode("utf-8")
+            pprint(part)
+        except OSError:
             break
-        command_output += part
-        if prompt in part:
+        print("=" * 40)
+        output += part
+        if prompt in output:
             break
-    return command_output
+    return output
 
 
 if __name__ == "__main__":
-    ip_list = ["192.168.100.1", "192.168.100.2", "192.168.100.3"]
-    for ip in ip_list:
-        out = cisco_send_show_commands(
-            ip, "cisco", "cisco", "cisco", ["sh ip int br", "sh clock"]
-        )
-        #pprint(out)
-        break
+    out = cisco_send_show_command(
+        "192.168.100.1", "cisco", "cisco", "cisco", "show ip int br",
+        prompt="#", read_timeout=10
+    )
+    pprint(out, width=120)
+
