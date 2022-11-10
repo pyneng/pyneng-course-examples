00ec0f9d354b5d56f218862575560b3b93a49074
pyneng <pyneng.course@gmail.com>
2022-11-10 10:45:50 +0000

    update 18 examples

--- examples/18_ssh_telnet/basics_05_paramiko_show.py
+++ examples/18_ssh_telnet/basics_05_paramiko_show.py
@@ -3,42 +3,37 @@ import time
 from pprint import pprint
 
 
-def cisco_send_show_command(
-    host, username, password, enable_pass, command, max_read=60000,
-    pause=0.5
+def cisco_get_show_output(
+    host, username, password, enable_pass, command, max_read=100000, pause=0.5
 ):
-    client = paramiko.SSHClient()
-    client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-    client.connect(
-        hostname=host,
-        username=username,
-        password=password,
-        look_for_keys=False,
-        allow_agent=False,
-    )
-    with client.invoke_shell() as ssh:
-        ssh.settimeout(5)
-        ssh.send("enable\n")
-        ssh.send(f"{enable_pass}\n")
-        time.sleep(pause)
-        ssh.send(f"terminal length 0\n")
-        time.sleep(pause)
-        ssh.recv(max_read)
+    with paramiko.SSHClient() as client:
+        client.load_system_host_keys()
+        # client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        client.connect(
+            hostname=host,
+            username=username,
+            password=password,
+            look_for_keys=False,
+            allow_agent=False,
+        )
+        with client.invoke_shell() as ssh:
+            ssh.settimeout(5)
+            ssh.send("enable\n")
+            ssh.send(f"{enable_pass}\n")
+            time.sleep(pause)
+            ssh.send("terminal length 0\n")
+            time.sleep(pause)
+            ssh.recv(max_read)
 
-        ssh.send(f"{command}\n")
-        time.sleep(pause)
-        output = ssh.recv(max_read)
-        return output.decode("utf-8")
+            ssh.send(f"{command}\n")
+            time.sleep(pause * 2)
+            output = ssh.recv(max_read)
+            return output.decode("utf-8").replace("\r\n", "\n")
 
 
 if __name__ == "__main__":
-    result = {}
-    ip_list = ["192.168.100.1", "192.168.100.2", "192.168.100.3"]
-    for ip in ip_list:
-        out = cisco_send_show_command(
-            ip, "cisco", "cisco", "cisco", "ping 8.8.8.8"
-        )
-        result[ip] = out
-        break
-    pprint(result)
+    out = cisco_get_show_output(
+        "192.168.100.1", "cisco", "cisco", "cisco", "sh run"
+    )
+    pprint(out, width=120)
 
