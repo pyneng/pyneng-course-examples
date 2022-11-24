fe4d2ed72aff2e38756dd4872b8d023cd25628ee
pyneng <pyneng.course@gmail.com>
2022-11-24 20:06:56 +0000

    examples 20 update

--- examples/20_jinja2/basics_03_syntax.py
+++ examples/20_jinja2/basics_03_syntax.py
@@ -3,10 +3,12 @@ from pprint import pprint
 import sys
 import os
 
-from jinja2 import Environment, FileSystemLoader
+from jinja2 import Environment, FileSystemLoader, StrictUndefined
 import yaml
+from rich import print as rprint
+from rich.rule import Rule
 
-
+# $ python basics_03_syntax.py templates/variables.txt data_files/vars.yml
 # $ python basics_03_syntax.py templates/for.txt data_files/for.yml
 template_dir, template_file = os.path.split(sys.argv[1])
 
@@ -16,10 +18,14 @@ env = Environment(
     loader=FileSystemLoader(template_dir),
     trim_blocks=True,
     lstrip_blocks=True,
+    # undefined=StrictUndefined,
 )
 template = env.get_template(template_file)
 
 with open(vars_file) as f:
     vars_dict = yaml.safe_load(f)
 
+# rprint(vars_dict)
+# rprint(Rule())
+print()
 print(template.render(vars_dict))
