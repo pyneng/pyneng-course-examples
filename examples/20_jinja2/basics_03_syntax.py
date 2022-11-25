97e7370121b0791e13cdb9241cf6a11ea5093432
pyneng <pyneng.course@gmail.com>
2022-11-25 08:22:45 +0000

    examples 20 update

--- examples/20_jinja2/basics_03_syntax.py
+++ examples/20_jinja2/basics_03_syntax.py
@@ -11,8 +11,12 @@ from rich.rule import Rule
 # $ python basics_03_syntax.py templates/variables.txt data_files/vars.yml
 # $ python basics_03_syntax.py templates/for.txt data_files/for.yml
 template_dir, template_file = os.path.split(sys.argv[1])
-
 vars_file = sys.argv[2]
+
+
+def add_0(number, zeroes=4):
+    return str(number).zfill(zeroes)
+
 
 env = Environment(
     loader=FileSystemLoader(template_dir),
@@ -20,6 +24,8 @@ env = Environment(
     lstrip_blocks=True,
     # undefined=StrictUndefined,
 )
+env.filters["add_zero"] = add_0
+# pprint(env.filters)
 template = env.get_template(template_file)
 
 with open(vars_file) as f:
