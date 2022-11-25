5839ad89ff7421fac032d75eca6d0a4454d5a706
pyneng <pyneng.course@gmail.com>
2022-11-25 12:51:05 +0000

    examples 21 update

--- examples/21_textfsm/basics_02_parse_output.py
+++ examples/21_textfsm/basics_02_parse_output.py
@@ -9,10 +9,10 @@ output_file = sys.argv[2]
 with open(output_file) as output:
     cmd_output = output.read()
 
-with open(template) as f:
-    re_table = textfsm.TextFSM(f)
-    header = re_table.header
-    result = re_table.ParseText(cmd_output)
+with open(template) as t:
+    fsm = textfsm.TextFSM(t)
+    header = fsm.header
+    result = fsm.ParseText(cmd_output)
     pprint(result, width=120)
     print()
     print(tabulate(result, headers=header))
