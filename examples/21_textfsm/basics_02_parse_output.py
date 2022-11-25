109e6d10d4095ca9b86df5199b1b5697968f9908
pyneng <pyneng.course@gmail.com>
2022-11-25 11:28:15 +0000

    examples 2 update

--- examples/21_textfsm/basics_02_parse_output.py
+++ examples/21_textfsm/basics_02_parse_output.py
@@ -6,9 +6,13 @@ from tabulate import tabulate
 template = sys.argv[1]
 output_file = sys.argv[2]
 
-with open(template) as f, open(output_file) as output:
+with open(output_file) as output:
+    cmd_output = output.read()
+
+with open(template) as f:
     re_table = textfsm.TextFSM(f)
     header = re_table.header
-    result = re_table.ParseText(output.read())
-    # result = re_table.ParseTextToDicts(output.read())
-    pprint([dict(zip(header, item)) for item in result], sort_dicts=False)
+    result = re_table.ParseText(cmd_output)
+    pprint(result, width=120)
+    print()
+    print(tabulate(result, headers=header))
---
+++ examples/21_textfsm/basics_03_ParseTextToDicts.py
@@ -0,0 +1,13 @@
+import sys
+from pprint import pprint
+import textfsm
+from tabulate import tabulate
+
+template = sys.argv[1]
+output_file = sys.argv[2]
+
+with open(template) as f, open(output_file) as output:
+    re_table = textfsm.TextFSM(f)
+    header = re_table.header
+    result = re_table.ParseTextToDicts(output.read())
+    pprint(result, sort_dicts=False)
