07b4f4c5b5eeda01d88fc7dd1e1d9c2ac628661e
pyneng <pyneng.course@gmail.com>
2022-12-15 06:27:59 +0000

    UPD examples 25

---
+++ examples/25_db/07_log.sql
@@ -0,0 +1,22 @@
+
+# 2022-12-15 06:26:31.055114
+select * from switch
++----------------+----------+------------+-------------------+
+| mac            | hostname | model      | location          |
++----------------+----------+------------+-------------------+
+| 0010.A1AA.C1CC | sw1      | Cisco 3750 | London, Green Str |
+| 0010.A1AA.21CC | <null>   | Cisco 3750 | London, Green Str |
+| 0020.A2AA.C2CC | sw2      | Cisco 3850 | London, Green Str |
+| 0030.A3AA.C3CC | sw3      | Cisco 3750 | London, Green Str |
+| 0040.A4AA.C4CC | sw4      | Cisco 3850 | London, Green Str |
+| 0050.A5AA.C5CC | sw5      | Cisco 3850 | London, Green Str |
+| 0060.A6AA.C6CC | sw6      | C3750      | London, Green Str |
+| 0070.A7AA.C7CC | sw7      | Cisco 3650 | London, Green Str |
+| 0110.A10A.C1CC | sw10     | 3750       | London, Green Str |
++----------------+----------+------------+-------------------+
+9 rows in set
+Time: 0.046s
+
+# 2022-12-15 06:27:44.999625
+.exit
+Goodbye!
