231d95784d4c47d313455e37f4359cab607acc80
pyneng <pyneng.course@gmail.com>
2022-12-15 06:06:10 +0000

    added short rib example

---
+++ examples/25_db/rib_schema.sql
@@ -0,0 +1,12 @@
+CREATE TABLE rib(
+  "status" TEXT,
+  "network" TEXT,
+  "netmask" INTEGER,
+  "nexthop" TEXT,
+  "metric" TEXT,
+  "locprf" TEXT,
+  "weight" INTEGER,
+  "path" TEXT,
+  "origin" TEXT
+);
+
