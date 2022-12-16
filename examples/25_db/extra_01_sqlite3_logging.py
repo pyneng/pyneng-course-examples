9d03d2de136829ed4628a719b512f79c5de94cf7
pyneng <pyneng.course@gmail.com>
2022-12-16 16:38:53 +0000

    UPD examples 25

---
+++ examples/25_db/extra_01_sqlite3_logging.py
@@ -0,0 +1,33 @@
+import sqlite3
+import logging
+
+
+logging.basicConfig(
+    format="{asctime} {name} {levelname} {message}",
+    datefmt="%H:%M:%S",
+    style="{",
+    level=logging.DEBUG,
+)
+
+
+data = [('0000.AAAA.CCCC', 'sw1', 'Cisco 3750', 'London, Green Str'),
+        ('0000.BBBB.CCCC', 'sw2', 'Cisco 3780', 'London, Green Str'),
+        ('0000.AAAA.DDDD', 'sw3', 'Cisco 2960', 'London, Green Str'),
+        ('0011.AAAA.CCCC', 'sw4', 'Cisco 3750', 'London, Green Str')]
+
+
+con = sqlite3.connect('sw_inventory2.db')
+con.set_trace_callback(logging.debug)
+
+con.execute('''create table if not exists switch
+            (mac text not NULL primary key, hostname text, model text, location text)'''
+            )
+
+query = 'INSERT into switch values (?, ?, ?, ?)'
+con.executemany(query, data)
+con.commit()
+
+for row in con.execute('select * from switch'):
+    print(row)
+
+con.close()
---
+++ examples/25_db/extra_02_sqlite3_rich_logging.py
@@ -0,0 +1,40 @@
+import sqlite3
+import logging
+from rich.logging import RichHandler
+
+
+logging.basicConfig(
+    format="{message}",
+    datefmt="%H:%M:%S",
+    style="{",
+    level=logging.DEBUG,
+    handlers=[RichHandler()]
+)
+
+data = [('0000.AAAA.CCCC', 'sw1', 'Cisco 3750', 'London, Green Str'),
+        ('0000.BBBB.CCCC', 'sw2', 'Cisco 3780', 'London, Green Str'),
+        ('0000.AAAA.DDDD', 'sw3', 'Cisco 2960', 'London, Green Str'),
+        ('0011.AAAA.CCCC', 'sw4', 'Cisco 3750', 'London, Green Str')]
+
+
+con = sqlite3.connect('sw_inventory2.db')
+con.set_trace_callback(logging.debug)
+
+con.execute('''create table if not exists switch
+            (mac text not NULL primary key,
+            hostname text,
+            model text,
+            location text)'''
+            )
+
+query = 'INSERT into switch values (?, ?, ?, ?)'
+#for row in data:
+#    with con:
+#        con.execute(query, row)
+
+con.executemany(query, data)
+
+for row in con.execute('select * from switch'):
+    print(row)
+
+con.close()
