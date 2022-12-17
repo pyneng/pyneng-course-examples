bf952348021a61b36a457f3de55a8e5ef89e5f74
pyneng <pyneng.course@gmail.com>
2022-12-17 10:00:11 +0000

    UPD examples 25

--- examples/25_db/basics_10_sqlite3_python.py
+++ examples/25_db/basics_10_sqlite3_python.py
@@ -1,7 +1,6 @@
 import sqlite3
 import logging
 
-
 logging.basicConfig(
     format="{asctime} {name} {levelname} {message}",
     datefmt="%H:%M:%S",
@@ -9,11 +8,30 @@ logging.basicConfig(
     level=logging.DEBUG,
 )
 
+data = [('0000.AAAA.CCCC', 'sw1', 'Cisco 3750', 'London, Green Str'),
+        ('0000.BBBB.CCCC', 'sw2', 'Cisco 3780', 'London, Green Str'),
+        ('0000.AAAA.DDDD', 'sw3', 'Cisco 2960', 'London, Green Str'),
+        ('0011.AAAA.CCCC', 'sw4', 'Cisco 3750', 'London, Green Str')]
 
-conn = sqlite3.connect("rib_database.db")
-conn.set_trace_callback(logging.debug)
+query_create = """
+create table switch (
+    mac text not NULL primary key,
+    hostname text,
+    model text,
+    location text
+)
+"""
+query_insert = 'INSERT into switch values (?, ?, ?, ?)'
+query_select = "SELECT * from switch"
 
-for row in conn.execute('select * from rib limit 10'):
-    print(row)
+con = sqlite3.connect("switch_inv_10.db")
+con.set_trace_callback(logging.debug)
+cursor = con.cursor()
 
-conn.close()
+cursor.execute(query_create)
+
+for row in data:
+	cursor.execute(query_insert, row)
+
+con.commit()
+con.close()
---
+++ examples/25_db/basics_11_sqlite3_execute_methods.py
@@ -0,0 +1,54 @@
+import sqlite3
+import logging
+
+logging.basicConfig(
+    format="{asctime} {name} {levelname} {message}",
+    datefmt="%H:%M:%S",
+    style="{",
+    level=logging.DEBUG,
+)
+
+query_create = """
+create table switch (
+    mac text not NULL primary key,
+    hostname text,
+    model text,
+    location text
+);
+create table admins (
+    email text not NULL primary key,
+    name text,
+    permissions integer CHECK (permissions in (0, 10, 20)) 
+);
+"""
+query_insert = "INSERT into switch values (?, ?, ?, ?)"
+query_insert_adm = "INSERT into admins values (:email, :name, :permissions)"
+query_select = "SELECT * from switch"
+
+data = [
+    ("0000.AAAA.CCCC", "sw1", "Cisco 3750", "London, Green Str"),
+    ("0000.BBBB.CCCC", "sw2", "Cisco 3780", "London, Green Str"),
+    ("0000.AAAA.DDDD", "sw3", "Cisco 2960", "London, Green Str"),
+    ("0011.AAAA.CCCC", "sw4", "Cisco 3750", "London, Green Str"),
+]
+
+admins = [
+    {"email": "john@gmail.com", "name": "John Brown", "permissions": 10},
+    {"email": "jessica@gmail.com", "name": "Jessica Newman", "permissions": 0},
+    {"email": "tom@gmail.com", "name": "Tom Green", "permissions": 55},
+]
+
+con = sqlite3.connect("switch_inv_11.db")
+con.set_trace_callback(logging.debug)
+cursor = con.cursor()
+
+cursor.executescript(query_create)
+
+for row in data:
+    cursor.execute(query_insert, row)
+
+for row_dict in admins:
+    cursor.execute(query_insert_adm, row_dict)
+
+con.commit()
+con.close()
