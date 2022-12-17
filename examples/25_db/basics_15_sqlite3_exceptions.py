4893203e532baf528984859e2c856708db60458b
pyneng <pyneng.course@gmail.com>
2022-12-17 19:12:37 +0000

    UPD examples 25

--- examples/25_db/basics_15_sqlite3_exceptions.py
+++ examples/25_db/basics_15_sqlite3_exceptions.py
@@ -24,14 +24,24 @@ data = [
     ("0000.BBBB.CCCC", "sw2", "Cisco 3780", "London, Green Str"),
     ("0000.AAAA.DDDD", "sw3", "Cisco 2960", "London, Green Str"),
     ("0011.AAAA.CCCC", "sw4", "Cisco 3750", "London, Green Str"),
+    ("0011.AAAA.CCCC", "sw5", "Cisco 3750", "London, Globe Str"),
 ]
 
-con = sqlite3.connect("switch_inv_14.db")
+con = sqlite3.connect("switch_inv_15.db")
 con.set_trace_callback(logging.debug)
 
 con.execute(query_create)
 
-with con:
-    con.executemany(query_insert, data)
+# with con:
+#     try:
+#         con.executemany(query_insert, data)
+#     except sqlite3.IntegrityError as error:
+#         print(error)
+
+try:
+    with con:
+        con.executemany(query_insert, data)
+except sqlite3.IntegrityError as error:
+    print(error)
 
 con.close()
--- examples/25_db/basics_16_sqlite3_exceptions_execute.py
+++ examples/25_db/basics_16_sqlite3_exceptions_execute.py
@@ -10,7 +10,7 @@ logging.basicConfig(
 
 
 query_create = """
-create table switch (
+create table if not exists switch (
     mac text not NULL primary key,
     hostname text,
     model text,
@@ -24,14 +24,29 @@ data = [
     ("0000.BBBB.CCCC", "sw2", "Cisco 3780", "London, Green Str"),
     ("0000.AAAA.DDDD", "sw3", "Cisco 2960", "London, Green Str"),
     ("0011.AAAA.CCCC", "sw4", "Cisco 3750", "London, Green Str"),
+    ("0011.AAAA.CCCC", "sw5", "Cisco 3750", "London, Globe Str"),
+    ("0030.AAAA.CCCC", "sw11", "Cisco 3750", "London, Green Str"),
+    ("0020.BBBB.CCCC", "sw12", "Cisco 3780", "London, Green Str"),
+    ("0070.AAAA.DDDD", "sw23", "Cisco 2960", "London, Green Str"),
 ]
 
-con = sqlite3.connect("switch_inv_14.db")
+con = sqlite3.connect("switch_inv_16.db")
 con.set_trace_callback(logging.debug)
 
 con.execute(query_create)
 
-with con:
-    con.executemany(query_insert, data)
+for row in data:
+    with con:
+        try:
+            con.execute(query_insert, row)
+        except sqlite3.IntegrityError as error:
+            print(error, row)
+
+# try:
+#     for row in data:
+#         with con:
+#             con.execute(query_insert, row)
+# except sqlite3.IntegrityError as error:
+#     print(error)
 
 con.close()
