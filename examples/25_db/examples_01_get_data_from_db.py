b58ab324302e0d0171da8cb19ec4597044f0960c
pyneng <pyneng.course@gmail.com>
2022-12-19 18:39:07 +0000

    UPD 25

--- examples/25_db/examples_01_get_data_from_db.py
+++ examples/25_db/examples_01_get_data_from_db.py
@@ -4,15 +4,27 @@ from pprint import pprint
 from tabulate import tabulate
 
 
+def get_table_headers(con, table):
+    headers = []
+    q = f"SELECT name FROM pragma_table_info('{table}')"
+    for row in con.execute(q):
+        headers.extend(row)
+    return headers
+
+
 #
 db_name = "net_interfaces_ex01.db"
 con = sqlite3.connect(db_name)
 
-rows = list(con.execute("select * from devices"))
+headers_dev = get_table_headers(con, "devices")
+dev_rows = list(con.execute("select * from devices"))
+headers_intf = get_table_headers(con, "interfaces")
 intf_rows = list(con.execute("select * from interfaces"))
 
 con.close()
 
 
-print(tabulate(rows))
-print(tabulate(intf_rows))
+print(tabulate(dev_rows, headers=headers_dev))
+print(tabulate(intf_rows, headers=headers_intf))
+# print(tabulate(dev_rows))
+# print(tabulate(intf_rows))
