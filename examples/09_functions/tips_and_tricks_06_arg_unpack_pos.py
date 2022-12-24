265761602f49dbaaf0266de2064d5d0b7a8e0836
pyneng <pyneng.course@gmail.com>
2022-12-24 08:02:31 +0000

    examples fix

--- examples/09_functions/tips_and_tricks_06_arg_unpack_pos.py
+++ examples/09_functions/tips_and_tricks_06_arg_unpack_pos.py
@@ -10,10 +10,10 @@ def config_interface(intf_name, ip_address, mask):
 
 # print(config_interface("Fa0/1", "10.0.1.1", "255.255.255.0"))
 interfaces_info = [
-    {"intf_name": "Fa0/1", "ip_address": "10.0.1.1", "mask": "255.255.255.0"},
-    {"intf_name": "Fa0/2", "ip_address": "10.0.2.1", "mask": "255.255.255.0"},
-    {"intf_name": "Fa0/3", "ip_address": "10.0.3.1", "mask": "255.255.255.0"},
-    {"intf_name": "Fa0/4", "ip_address": "10.0.4.1", "mask": "255.255.255.0"},
+    ["Fa0/1", "10.0.1.1", "255.255.255.0"],
+    ["Fa0/2", "10.0.2.1", "255.255.255.0"],
+    ["Fa0/3", "10.0.3.1", "255.255.255.0"],
+    ["Fa0/4", "10.0.4.1", "255.255.255.0"],
 ]
 
 for info in interfaces_info:
